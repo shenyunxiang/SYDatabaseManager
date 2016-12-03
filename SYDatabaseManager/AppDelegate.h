fd1b58c34c1a1c6a0a7881caa6a8220166aa1c80
shenxunxiang <shenyunxiang@shenyunxiangdeMacBook-Pro.local>
2016-12-03 15:30:15 +0800

    init

---
+++ SYDatabaseManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SYDatabaseManager
+//
+//  Created by 沈云翔 on 2016/12/3.
+//  Copyright © 2016年 shenyunxiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SYDatabaseManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SYDatabaseManager
+//
+//  Created by 沈云翔 on 2016/12/3.
+//  Copyright © 2016年 shenyunxiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
